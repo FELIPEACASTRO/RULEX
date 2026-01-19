94b207b03f4bfa7630ef18326eadc1de36e1eea1
Felipe Angelo <felipesp1983@hotmail.com>
2026-01-19 16:20:08 +0000

    chore(cleanup): remover 17 arquivos Java órfãos
    CLEAN-001: Remoção de código morto identificado na auditoria.
    
    Removidos:
    - 6 entities não utilizadas (PanLocationHistory, VelocityMetrics, BloomFilterMetadata, ShadowEvaluationLog, RuleAbTest, RuleAbTestAssignment)
    - 6 repositories correspondentes
    - 4 arquivos do sistema de roles homolog não integrado (RoleEntity, UserRoleEntity, RoleRepository, UserRoleRepository)
    - 1 utilitário SensitiveDataMasker não utilizado
    
    As tabelas no banco permanecem (migrations preservadas).
    Build: 1477 testes passando.

--- backend/src/main/java/com/rulex/repository/homolog/UserRoleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.rulex.repository.homolog;
-
-import com.rulex.entity.homolog.UserRoleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRoleEntity, UserRoleEntity.Pk> {}
